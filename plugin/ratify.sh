b83536a529334a7cb1811cdb96c5fcf7aa59dbce
Akash Singhal <akashsinghal@microsoft.com>
2024-07-09 18:09:04 -0700

    feat: add extra config capabilities and other fixes (#3)

--- plugin/ratify.sh
+++ plugin/ratify.sh
@@ -3,6 +3,10 @@
 name=$2
 digest=$4
 export HOME=/root
+if [ ! -f ~/.ratify/config.json ]; then
+    echo "ratify config file not found. failing open"
+    exit 0
+fi
 ratifyOutput=$(~/.ratify/bin/ratify verify -c ~/.ratify/config.json -s $name --digest $digest)
 isSuccess=$(echo $ratifyOutput | jq .isSuccess)
 echo $ratifyOutput | jq . 
